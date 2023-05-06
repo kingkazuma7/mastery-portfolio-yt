eaa35f3b5f5ab13db644411facd2070a0d5f1a9c
kingkazuma7 <your.nasionasio7@gmail.com>
2023-05-06 10:47:35 +0900

    npx prettier --write .

--- backend_sanity/mastery-portfolio-yt/sanity.cli.ts
+++ backend_sanity/mastery-portfolio-yt/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'qs6rsi74',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
