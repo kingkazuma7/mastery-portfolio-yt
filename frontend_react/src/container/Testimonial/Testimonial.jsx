320a461a82645d32ff88d539d26b39efe1902d15
kingkazuma7 <your.nasionasio7@gmail.com>
2023-05-25 20:13:55 +0900

    空のTestimonialsまで

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 import "./Testimonial.scss";
 
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
+
 const Testimonial = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [testimonials, settesTimonials] = useState([])
+  const [brands, setBrands] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "testimonial"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(query)
+      .then((data) => {
+        settesTimonials(data);
+    });
+
+    client.fetch(brandsQuery)
+    .then((data) => {
+      setBrands(data);
+    });
+  }, []);
+
   return (
-    <div>Testimonial</div>
+    <>
+      Testimonials
+    </>
   )
 }
 
-export default Testimonial
+export default AppWrap(
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'app__testimonial',
+  'app__primarybg',
+);
