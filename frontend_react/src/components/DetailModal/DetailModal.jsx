13488a70acbc1760d7cc64652aae5aeaee2a5fda
kingkazuma7 <your.nasionasio7@gmail.com>
2024-02-05 14:30:06 +0900

    モーダル表示時は背景スクロールを止める

--- frontend_react/src/components/DetailModal/DetailModal.jsx
+++ frontend_react/src/components/DetailModal/DetailModal.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './DetailModal.scss';
 import Modal from 'react-modal';
 import BlockContent from '@sanity/block-content-to-react';
 
 const DetailModal = ({ isOpen, onRequestClose, selectedWorkDetails }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [isOpen]);
+
   if (!isOpen) {
     return null;
   }
