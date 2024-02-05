41e1bd72dd43f5ed473a00ddcd52eb155ce77817
kingkazuma7 <your.nasionasio7@gmail.com>
2024-02-05 14:23:55 +0900

    テキストエリアを@sanity/block-content-to-reactに変更

--- frontend_react/src/components/DetailModal/DetailModal.jsx
+++ frontend_react/src/components/DetailModal/DetailModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './DetailModal.scss';
 import Modal from 'react-modal';
+import BlockContent from '@sanity/block-content-to-react';
 
 const DetailModal = ({ isOpen, onRequestClose, selectedWorkDetails }) => {
   if (!isOpen) {
@@ -13,7 +14,7 @@ const DetailModal = ({ isOpen, onRequestClose, selectedWorkDetails }) => {
       onRequestClose={onRequestClose}
       contentLabel="Work Details"
     >
-      <p dangerouslySetInnerHTML={{ __html: selectedWorkDetails }}></p>
+      <BlockContent blocks={selectedWorkDetails} />
       <button onClick={onRequestClose}>閉じる</button>
     </Modal>
   )
